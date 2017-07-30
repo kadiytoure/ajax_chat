36acc96fec64b8db95b301af07b041c37ccd19fa
toure <tourekadiy@gmail.com>
2017-07-30 20:33:23 +0200

    ajout dans le fichier  new-message.php de l'objet message;

--- new-message.php
+++ new-message.php
@@ -14,3 +14,36 @@ echo $_POST['message'];
     header('Content-Type: text/plain');
     echo 'expect a message parameter';
 }
+
+class Message {
+//creation object message
+
+private $content;
+private $time;
+
+
+
+
+function __construct($content, $time) {
+    $this->content = $content;
+    $this->time = $time;
+  
+  
+}
+function getContent() {
+    return $this->content;
+}
+
+function getTime() {
+    return $this->time;
+}
+
+function genererhtmlmessage(){
+    echo '<h2>Today<h2>';
+    echo '<section>';
+    echo '<h3> time: </h3><p>' . $this->time . '</p>';
+    echo '<h3> content: </h3><p>' . $this->content . '</p>';
+    echo '</section>';
+}
+
+}
