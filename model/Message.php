f1f1693d982165fc8ad065725e228827b03e0cb2
toure <tourekadiy@gmail.com>
2017-07-31 22:08:07 +0200

    mise en place de la classe message dans dossier modèle;

---
+++ model/Message.php
@@ -0,0 +1,11 @@
+<?php
+class Message {
+    public $text;
+    public $timestamp;
+
+ public function __construct($text) {
+        $this->text = $text;
+        $this->timestamp = new DateTime();
+    }
+}
+?>
