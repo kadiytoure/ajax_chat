f4e3aea2b1b7a3dcd4226dd066426590fec492ea
toure <tourekadiy@gmail.com>
2017-08-22 14:41:25 +0200

    réglages database;

--- ajax-chat.sql
+++ ajax-chat.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS `kadiy_ajax_chat`;
 CREATE DATABASE `kadiy_ajax_chat` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-#DROP USER 'ajax_chat_user'@'localhost';
-#CREATE USER 'ajax_chat_user'@'localhost' IDENTIFIED BY 'API SQL';
-#GRANT ALL PRIVILEGES ON `ajax_chat`.* TO 'ajax_chat_user'@'localhost';
+DROP USER 'ajax_chat_user'@'localhost';
+CREATE USER 'kadiy'@'localhost' IDENTIFIED BY 'kadiy';
+GRANT ALL PRIVILEGES ON `kadiy_ajax_chat`.* TO 'kadiy'@'localhost';
 USE `kadiy_ajax_chat`;
 
 CREATE TABLE `message` (
